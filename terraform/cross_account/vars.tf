d2b539914b870ef9f257c885c233d189429bacd3
Aidan Feldman <aidan.feldman@gsa.gov>
2019-11-17 22:34:29 -0500

    update AWS account list

--- terraform/cross_account/vars.tf
+++ terraform/cross_account/vars.tf
@@ -1,6 +1,6 @@
 variable "dest_account_numbers" {
   description = "The account numbers you want to be able to switch into."
-  # should include everything in
+  # should include all active accounts in
   # https://docs.google.com/spreadsheets/d/1DedSCiU9AsCAAVvAFZT0_Ii7AFIKlI-JNifzlpHNbDg/edit#gid=0
   default = [
     "001907687576",
@@ -12,9 +12,8 @@ variable "dest_account_numbers" {
     "213305845712",
     "312530187933",
     "340731855345",
-    "405436375165",
     "461353137281",
-    "469931042344",
+    "533787958253",
     "541873662368",
     "555546682965",
     "560284223511",
@@ -22,8 +21,8 @@ variable "dest_account_numbers" {
     "587807691409",
     "699351240001",
     "765358534566",
+    "810504390172",
     "894947205914",
-    "992476103436",
   ]
 }
 
