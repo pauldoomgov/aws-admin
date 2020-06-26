2edb005361d9a069a9b16560189acc44a915fa96
Aidan Feldman <aidan.feldman@gsa.gov>
2020-06-26 13:14:26 -0400

    create cloud.gov sandbox using module

--- terraform/cloud_gov.tf
+++ terraform/cloud_gov.tf
@@ -19,16 +19,13 @@ module "cloud_gov_jump" {
   cross_account_role_name = var.role_name
 }
 
-resource "aws_organizations_account" "cloud_gov_sandbox" {
-  provider = aws.payer
-
-  name                       = "tts-cloudgov-sandbox"
-  email                      = "devops+aws-cloudgov-sandbox@gsa.gov"
-  iam_user_access_to_billing = "ALLOW"
-  parent_id                  = module.cloud_gov.org_unit_id
-
-  lifecycle {
-    ignore_changes  = [email, role_name]
-    prevent_destroy = true
+module "cloud_gov_sandbox" {
+  source = "./account"
+  providers = {
+    aws = aws.payer
   }
+
+  name                    = "tts-cloudgov-sandbox"
+  org_unit_id             = module.cloud_gov.org_unit_id
+  cross_account_role_name = var.role_name
 }
