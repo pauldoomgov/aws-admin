25c0bd3bcd9d9e2fa684242ab76fec78c23a213d
Aidan Feldman <aidan.feldman@gsa.gov>
2020-06-26 11:24:51 -0400

    set up admins group in cloud.gov with Terraform

--- terraform/cloud_gov.tf
+++ terraform/cloud_gov.tf
@@ -11,17 +11,62 @@ module "cloud_gov" {
 resource "aws_organizations_account" "cloud_gov_jump" {
   provider = aws.payer
 
-  name  = "tts-cloudgov-jump"
-  email = "devops+aws-cloudgov-jump@gsa.gov"
+  name                       = "tts-cloudgov-jump"
+  email                      = "devops+aws-cloudgov-jump@gsa.gov"
   iam_user_access_to_billing = "ALLOW"
-  parent_id = module.cloud_gov.org_unit_id
+  parent_id                  = module.cloud_gov.org_unit_id
+
+  lifecycle {
+    ignore_changes  = [email, role_name]
+    prevent_destroy = true
+  }
 }
 
 resource "aws_organizations_account" "cloud_gov_sandbox" {
   provider = aws.payer
 
-  name  = "tts-cloudgov-sandbox"
-  email = "devops+aws-cloudgov-sandbox@gsa.gov"
+  name                       = "tts-cloudgov-sandbox"
+  email                      = "devops+aws-cloudgov-sandbox@gsa.gov"
   iam_user_access_to_billing = "ALLOW"
-  parent_id = module.cloud_gov.org_unit_id
+  parent_id                  = module.cloud_gov.org_unit_id
+
+  lifecycle {
+    ignore_changes  = [email, role_name]
+    prevent_destroy = true
+  }
+}
+
+provider "aws" {
+  alias  = "cloud_gov_jump"
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.cloud_gov_jump.id}:role/${var.role_name}"
+  }
+}
+
+resource "aws_iam_group" "cloud_gov_jump_admins" {
+  provider = aws.cloud_gov_jump
+
+  name = "Administrators"
+}
+
+locals {
+  admin_policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "cloud_gov_jump_admin" {
+  provider = aws.cloud_gov_jump
+
+  group      = aws_iam_group.cloud_gov_jump_admins.name
+  policy_arn = local.admin_policy_arn
+}
+
+resource "aws_iam_user" "peter_burkholder" {
+  name = "peter.burkholder@gsa.gov"
+}
+
+resource "aws_iam_user_group_membership" "peter_burkholder_admin" {
+  user   = aws_iam_user.peter_burkholder.name
+  groups = [aws_iam_group.cloud_gov_jump_admins.name]
 }
