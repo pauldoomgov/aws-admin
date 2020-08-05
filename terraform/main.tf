5f33c5c7de11b229dc17ffd415dffdeb673f710f
Aidan Feldman <aidan.feldman@gsa.gov>
2020-08-05 12:53:54 -0400

    split jump account setup to its own file

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,20 +20,3 @@ provider "aws" {
 resource "aws_iam_group" "admins" {
   name = "tts-tech-portfolio-admins"
 }
-
-data "aws_iam_policy_document" "cross_account" {
-  statement {
-    actions   = ["sts:AssumeRole"]
-    resources = [for acct in data.aws_organizations_organization.main.accounts : "arn:aws:iam::${acct.id}:role/${var.role_name}"]
-  }
-}
-
-resource "aws_iam_policy" "assume_role" {
-  name   = "allow-assume-cross-account-role"
-  policy = data.aws_iam_policy_document.cross_account.json
-}
-
-resource "aws_iam_group_policy_attachment" "assume_role" {
-  group      = aws_iam_group.admins.name
-  policy_arn = aws_iam_policy.assume_role.arn
-}
