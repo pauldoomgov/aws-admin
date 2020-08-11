29910c999eb1d693051dac71e97765048a872456
John Jediny <JJediny@users.noreply.github.com>
2020-08-11 14:49:37 -0400

    temp comment out 2nd step and rename module per review

--- terraform/coe_forms.tf
+++ terraform/coe_forms.tf
@@ -5,15 +5,15 @@ module "tts_faas_dev" {
   }
 
   name        = "tts-faas-dev"
-  org_unit_id = module.solutions.org_unit_id
+  org_unit_id = module.coe.org_unit_id
 }
 
-module "tts_faas_dev_setup" {
-  source = "./account_setup"
-
-  account_id              = module.tts_faas_dev.account_id
-  cross_account_role_name = var.role_name
-}
+#module "tts_faas_dev_setup" {
+#  source = "./account_setup"
+#
+#  account_id              = module.tts_faas_dev.account_id
+#  cross_account_role_name = var.role_name
+#}
 
 module "tts_faas_test" {
   source = "./account"
@@ -22,15 +22,15 @@ module "tts_faas_test" {
   }
 
   name        = "tts-faas-test"
-  org_unit_id = module.solutions.org_unit_id
+  org_unit_id = module.coe.org_unit_id
 }
 
-module "tts_faas_test_setup" {
-  source = "./account_setup"
-
-  account_id              = module.tts_faas_test.account_id
-  cross_account_role_name = var.role_name
-}
+#module "tts_faas_test_setup" {
+#  source = "./account_setup"\
+#
+#  account_id              = module.tts_faas_test.account_id
+#  cross_account_role_name = var.role_name
+#}
 
 module "tts_faas_prod" {
   source = "./account"
@@ -39,12 +39,12 @@ module "tts_faas_prod" {
   }
 
   name        = "tts-faas-prod"
-  org_unit_id = module.solutions.org_unit_id
+  org_unit_id = module.coe.org_unit_id
 }
 
-module "tts_faas_prod_setup" {
-  source = "./account_setup"
-
-  account_id              = module.tts_faas_prod.account_id
-  cross_account_role_name = var.role_name
-}
+#module "tts_faas_prod_setup" {
+#  source = "./account_setup"
+#
+#  account_id              = module.tts_faas_prod.account_id
+#  cross_account_role_name = var.role_name
+#}
