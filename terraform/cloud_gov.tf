b59e685d6b2737cae5674cf39c7576fa234ccd28
John Jediny <JJediny@users.noreply.github.com>
2020-08-11 15:00:24 -0400

    temp comment account setup and run terraform apply

--- terraform/cloud_gov.tf
+++ terraform/cloud_gov.tf
@@ -49,12 +49,12 @@ module "federalist" {
   }
 
   name        = "federalist"
-  org_unit_id = module.solutions.org_unit_id
+  org_unit_id = module.cloud_gov.org_unit_id
 }
 
-module "federalist_setup" {
-  source = "./account_setup"
-
-  account_id              = module.federalist.account_id
-  cross_account_role_name = var.role_name
-}
+#module "federalist_setup" {
+#  source = "./account_setup"
+#
+#  account_id              = module.federalist.account_id
+#  cross_account_role_name = var.role_name
+#}
