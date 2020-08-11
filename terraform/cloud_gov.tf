48af0ce603fbc1d0d43684600e583eea6d0b94c2
John Jediny <JJediny@users.noreply.github.com>
2020-08-11 16:42:40 -0400

    uncommenting account-setup in advance of completing manual steps

--- terraform/cloud_gov.tf
+++ terraform/cloud_gov.tf
@@ -52,9 +52,9 @@ module "federalist" {
   org_unit_id = module.cloud_gov.org_unit_id
 }
 
-#module "federalist_setup" {
-#  source = "./account_setup"
-#
-#  account_id              = module.federalist.account_id
-#  cross_account_role_name = var.role_name
-#}
+module "federalist_setup" {
+  source = "./account_setup"
+
+  account_id              = module.federalist.account_id
+  cross_account_role_name = var.role_name
+}
