f50c6d87cb15d315e5b33166965f8f88cb1a3deb
Aidan Feldman <aidan.feldman@gsa.gov>
2020-08-10 22:59:06 -0400

    move template into terraform file
    Not any less readable, and it provides syntax highlighting.

--- terraform/local_config.tf
+++ terraform/local_config.tf
@@ -3,7 +3,18 @@ provider "local" {
 }
 
 resource "local_file" "aws_config" {
-  content         = templatefile("${path.module}/aws_config.ini.tmpl", { accounts = data.aws_organizations_organization.main.accounts, role_name = var.role_name })
+  # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html#cli-configure-files-settings
+  # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-role.html
+  content = <<EOT
+[default]
+region = us-east-1
+%{for account in data.aws_organizations_organization.main.accounts}
+[profile ${replace(lower(account.name), "/\\W/", "-")}]
+role_arn = arn:aws:iam::${account.id}:role/${var.role_name}
+source_profile = default
+%{endfor~}
+EOT
+
   filename        = "${path.module}/aws_config.ini"
   file_permission = "0600"
 }
