e9b427c08d42cd5978b87de4129464bd101fcf53
Aidan Feldman <aidan.feldman@gsa.gov>
2020-09-15 09:40:38 -0400

    add script to run AWS CLI commands across profiles

---
+++ bin/all_profiles.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Runs an AWS CLI subcommand across all profiles in your AWS configuration file by replacing `aws` with `./all_profiles.py`.
+#
+#   ./all_profiles.py [subcommand] [args]
+#
+# Example use:
+#
+#   AWS_DEFAULT_REGION=us-gov-west-1 ./all_profiles.py ec2 describe-instances
+
+import boto3
+import sys
+import subprocess
+
+session = boto3.Session()
+
+for profile in session.available_profiles:
+    print("+ PROFILE:", profile)
+
+    cmd = ["aws", "--profile", profile] + sys.argv[1:]
+    subprocess.run(cmd)
